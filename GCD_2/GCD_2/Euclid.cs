519c7db8d8efa4289848ff67ac4664c19f4c0090
senyak <mselina@sfedu.ru>
2023-12-14 01:24:59 +0300

    tests...

---
+++ GCD_2/GCD_2/Euclid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCD_2
+{
+    public class Euclid
+    {
+        public static int GCD(int a, int b)
+        {
+            if (a == 0 && b == 0)
+                throw new ArgumentException("all numbers are 0 at the same time ");
+
+            while (b != 0)
+            {
+                var t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Алгоритм Евклида");
+            //IM GONNA RUIN THIS PROJECT
+            Console.WriteLine("Наибольший общий делитель чисел {0} и {1} равен {2}", 294, 162, GCD(294, 162));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GCD_2/TestGCD2/UnitTest1.cs
@@ -0,0 +1,34 @@
+using GCD_2;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace TestGCD2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodEqual()
+        {
+            Assert.AreEqual(Euclid.GCD(1, 10), 1);
+            Assert.AreEqual(Euclid.GCD(2, 10), 2);
+            Assert.AreEqual(Euclid.GCD(121, 11), 11);
+            Assert.AreEqual(Euclid.GCD(121, 33), 11);
+            Assert.AreEqual(Euclid.GCD(121, 33), 11);
+        }
+
+        [TestMethod]
+        public void TestMethodNOTEqual()
+        {
+            Assert.AreNotEqual(Euclid.GCD(1, 10), 5);
+            Assert.AreNotEqual(Euclid.GCD(2, 10), 10);
+            Assert.AreNotEqual(Euclid.GCD(121, 11), 121);
+            Assert.AreNotEqual(Euclid.GCD(121, 33), 23);
+        }
+
+        [TestMethod]
+        public void TestMethodException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => { Euclid.GCD(0, 0); });
+        }
+    }
+}
